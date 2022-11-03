5122952173a5aea455574cec73b9451701bffa02
dev-acc-practice <development-practice@proton.me>
2022-11-03 12:14:11 +0200

    installed tailwind and created header

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'palePink': 'rgb(255,247,241)',
+        'lightGray': 'rgb(139,133,126)'
+      },
+    },
+  },
+  plugins: [],
+}
