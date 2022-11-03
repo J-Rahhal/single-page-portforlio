e197a71db61b08a0426ef117da9a355d7f9b7dc0
dev-acc-practice <development-practice@proton.me>
2022-11-03 14:30:34 +0200

    created and styled categories row

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     extend: {
       colors: {
         'palePink': 'rgb(255,247,241)',
-        'lightGray': 'rgb(139,133,126)'
+        'lightGray': 'rgb(139,133,126)',
+        'violet': 'rgb(117,92,221)',
+        'softOrange': 'rgb(245,166,97)',
+        'lightPink': 'rgb(243,157,159)',
+        'veryLightRed': 'rgb(233,118,100)',
+        'veryPaleGreen': 'rgb(98,196,183)',
+        'darkPurple': 'rgb(86,31,75)',
       },
     },
   },
